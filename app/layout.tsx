b76cfa2b13f9c124d46e1af538e6fea07991c672
Confidence Emonena <confidinho@yahoo.com>
2025-09-24 12:23:37 +0100

    google tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import { sequelFont } from "./fonts";
 import "./globals.css";
 
@@ -30,8 +31,25 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<Script id="gtm-script" strategy="afterInteractive">
+					{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5CTVJ94W');`}
+				</Script>
+			</head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} ${sequelFont.variable} antialiased`}>
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-5CTVJ94W"
+						height="0"
+						width="0"
+						style={{ display: "none", visibility: "hidden" }}
+					/>
+				</noscript>
 				{children}
 				<Analytics />
 			</body>
